aa1c7bb3937409a3ee9ed32e53a519e40ee7c770
Matthew Bird <ptacidlo@gmail.com>
2024-03-16 13:58:09 +0100

    Add for loop and wanted arguments to the function
    First argument is an array with number
    Second argument is used as rest argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(numberArray, ...num) {
 
+    let newArray = [];
+
+    for (let i = 0; i < numberArray.length; i++) {
+        if (!num.includes(numberArray[i])) {
+            newArray.push(numberArray[i])
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
