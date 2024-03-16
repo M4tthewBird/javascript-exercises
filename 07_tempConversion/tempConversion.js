8ff3964500c55ac651fb12ab508e0dee70d05ea5
Matthew Bird <ptacidlo@gmail.com>
2024-03-16 14:46:44 +0100

    Add conversion equation to both functions
    Added conversion and round logic to both functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+
+  celsius = (number - 32) * 5/9
+
+  return Math.round(celsius * 10) / 10
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+
+  fahrenheit = number * 9/5 + 32
+
+  return Math.round(fahrenheit * 10) / 10
+
 };
 
 // Do not edit below this line
