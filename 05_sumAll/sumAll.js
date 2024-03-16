75536dcefec063a6a930591d4b1bd5e1d6fcfe6e
Matthew Bird <ptacidlo@gmail.com>
2024-03-16 14:16:37 +0100

    Add necessary logic
    Created if condition for checking if the input is integer
    Created condition if the input is non negative
    Created condition which checks if the num1 is bigger than num2
    Created for loop which sums the numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumNumber = 0;
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR"
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+
+    if (num1 > num2) {
+        const tempVar = num1;
+        num1 = num2;
+        num2 = tempVar;
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        sumNumber += i;
+    }
+    
+    return sumNumber
 };
 
 // Do not edit below this line
