091af0a1522b08bd6ec035f99e911452a9823777
Matthew Bird <ptacidlo@gmail.com>
2024-03-16 13:17:34 +0100

    Add logic and loop
    Added if to check if the number of reps is not negative
    Added loop to add the right amount of strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let returnString = "";
+    
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            returnString = returnString.concat(string)
+        }
+        return returnString
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
