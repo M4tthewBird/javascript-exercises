812d2a847146b6cc46665ca389b45abf67edd29b
Matthew Bird <ptacidlo@gmail.com>
2024-04-12 11:49:29 +0200

    Add functionality

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    let number
+    if (typeof num !== 'number') {
+        number = parseInt(num)
+    } else {
+        number = num
+    }
+
+    if (number < 0) {
+        return "OOPS"
+    } else if (number == 0 ) {
+        return 0
+    } else {
+
+        let firstPrev = 1;
+        let secondPrev = 0;
+
+        for (let i = 2; i <= number; i++) {
+            let current = firstPrev + secondPrev;
+            secondPrev = firstPrev;
+            firstPrev = current;
+        }
+
+        return firstPrev;
+    }
 
 };
 
