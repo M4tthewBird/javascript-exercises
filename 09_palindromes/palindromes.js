037b929359437e180da244cf73c94c1de5ccf4e2
Matthew Bird <ptacidlo@gmail.com>
2024-04-12 11:30:30 +0200

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+
+    const lowerWord = word
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const reversedWord = lowerWord.split("").reverse().join("")
+
+    return lowerWord === reversedWord
 };
 
 // Do not edit below this line
